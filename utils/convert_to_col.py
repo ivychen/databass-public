f50ee78a479f85bed2c86a652de80396130f38b6
Wangshu Hong <wh2459@columbia.edu>
2020-12-07 10:02:08 +0800

    add utils to convert a table into seperate csv files

---
+++ utils/convert_to_col.py
@@ -0,0 +1,38 @@
+import pandas
+import os
+
+# for root, dirs, files in os.walk("."):
+
+def load_data(file: str):
+    for sep in [',', '|', '\t']:
+        df = None
+        try:
+            with open(file) as f:
+                df = pandas.read_csv(f, sep=sep)
+                return df
+        except Exception as e:
+            exception = e
+            print(e)
+
+# generate column files with conversion: {table name}-{column name}.csv
+def generate_column_files(df: pandas.DataFrame, fname: str):
+    for column in df: 
+        print('Colunm name:', column)
+        data = df[column]
+        output_name = fname + "-" + column + ".csv"
+        data.to_csv(output_name, header=False, index=False)
+
+
+def load_and_generate():
+    path = os.path.join("..", "col_data")
+    for fname in os.listdir(path):
+        print(fname)
+        if fname.endswith(".tbl"):
+            fpath = os.path.join(path, fname)
+            df = load_data(fpath)
+            table = fname.rstrip(".tbl")
+            generate_column_files(df, table)
+
+
+if __name__ == "__main__":
+    load_and_generate()
